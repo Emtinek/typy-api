c92dff77e1f5b4cc86e5975abb5464554a042fad
Blazej Duda <inkrinyt@gmail.com>
2025-07-20 16:07:30 +0200

    Poprawoiona wersja app.py - endpoint json

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from forebet_scraper import get_forebet_predictions
+import json
 
 app = Flask(__name__)
 
@@ -7,7 +8,7 @@ app = Flask(__name__)
 def typy():
     get_forebet_predictions()
     with open("matches.json", encoding="utf-8") as f:
-        data = f.read()
+        data = json.load(f)
     return jsonify(data)
 
 if __name__ == "__main__":
