67b9378853fa44341f5315d3012f4cee1ddbafcd
Blazej Duda <inkrinyt@gmail.com>
2025-07-20 16:11:45 +0200

    bo tak

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask, jsonify
-from forebet_scraper import get_forebet_predictions
 import json
 
 app = Flask(__name__)
 
 @app.route("/typy")
 def typy():
-    get_forebet_predictions()
     with open("matches.json", encoding="utf-8") as f:
         data = json.load(f)
     return jsonify(data)
