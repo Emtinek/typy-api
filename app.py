ca6c4d88fe9717f28064619f165cadf3958f517d
Blazej Duda <inkrinyt@gmail.com>
2025-07-20 15:24:19 +0200

    Poprawka dodano poprawny start programu

--- app.py
+++ app.py
@@ -3,7 +3,12 @@ from forebet_scraper import get_forebet_predictions
 
 app = Flask(__name__)
 
-@app.route(typy)
-def typy()
-    matches = get_forebet_predictions()
-    return jsonify(matches)
+@app.route("/typy")
+def typy():
+    get_forebet_predictions()
+    with open("matches.json", encoding="utf-8") as f:
+        data = f.read()
+    return jsonify(data)
+
+if __name__ == "__main__":
+    app.run()
